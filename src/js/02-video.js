fc0e034bfbac6befb555595ae29fa3ad317fc8a2
ludmila121 <113711608+ludmila121@users.noreply.github.com>
2023-03-09 20:59:08 +0100

    666

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+ 
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
 
+    player.on('play', function() {
+        console.log('played the video!')
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title)
+    });
+   const onPlay = function(time) {
+    
+};
+
+player.on('timeupdate', onPlay);
+player.on('eventName', function(data) {
+
+});
+{
+    duration: 61.857
+    percent: 0.049
+    seconds: 3.034
+}
