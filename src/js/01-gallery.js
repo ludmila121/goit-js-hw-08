46f6c6ba2110b1efcf9779f1375043d89a5e6c3a
ludmila121 <113711608+ludmila121@users.noreply.github.com>
2023-03-14 16:17:13 +0100

    111

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,20 +1,23 @@
 // Add imports above this line
 import simpleLightbox from 'simplelightbox';
-import 'simpleLightbox/dist/simple-lightbox.min.css';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 const galleryListRef = document.querySelector('.gallery');
-const makeGalleryListMarkup = ({ original, preview, description}) => {
+const makeGalleryListMarkup = ({ original, preview, description }) => {
   return `
      <ul class="gallery"><a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
 </a></ul>
   `;
 };
-const makeGalleryListElement = galleryItems.map(makeGalleryListMarkup).join("");
-galleryListRef.insertAdjacentHTML("beforeend", makeGalleryListElement);
+const makeGalleryListElement = galleryItems.map(makeGalleryListMarkup).join('');
+galleryListRef.insertAdjacentHTML('beforeend', makeGalleryListElement);
 
-const lightbox = new SimpleLightbox('.gallery a', { captionsData: `alt`, captionPosition: 'bottom', captionDelay: 250 });
-
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: `alt`,
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
